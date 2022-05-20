609c2ffd4a479baccb28dae8bcdb40fbb1794519
Kashyap Panda <5233012+kashyappanda@users.noreply.github.com>
2022-05-19 23:51:30 -0700

    Added room creation and database sync

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const path = require('path');
 const roomIdGenerator = require('./util/roomIdGenerator.js');
 const mongoose = require('mongoose');
 const config = require('config');
+const Room = require("./models/Rooms");
 
 
 // import handlers
@@ -16,24 +17,24 @@ const app = express();
 const port = 8080;
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // If you choose not to use handlebars as template engine, you can safely delete the following part and use your own way to render content
 // view engine setup
-app.engine('hbs', hbs.engine({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}));
+app.engine('hbs', hbs.engine({ extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/' }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 // Connect to MonboDB
 const db = config.get('mongoURI');
-console.log("Connecting to MongoDB");
+console.log("Connecting to MongoDB...");
 mongoose.connect(db, err => {
     if (err) {
         throw err;
     }
-    console.log("Connected to MongoDB");
+    console.log("Connected to MongoDB!");
 });
 
 // set up stylesheets route
@@ -41,8 +42,29 @@ mongoose.connect(db, err => {
 // TODO: Add server side code
 
 // Create controller handlers to handle requests at each endpoint
+//getRoom - Returns all rooms in JSON format
+app.get("/getRoom", function (req, res) {
+    Room.find().lean().then(item => {
+        res.json(item);
+    });
+});
+
+// Create endpoint - Creates a new room in the database
+app.post("/create", function (req, res) {
+    const newRoom = new Room ({
+        name: req.body.roomName,
+        id: roomIdGenerator.roomIdGenerator()
+    });
+
+    newRoom.save().then(console.log("New room created")).catch(err => console.log("Error creating room"));
+});
+
+//Main page
 app.get('/', homeHandler.getHome);
+
+//Specific Room
 app.get('/:roomName', roomHandler.getRoom);
+
 
 // NOTE: This is the sample server.js code we provided, feel free to change the structures
 
